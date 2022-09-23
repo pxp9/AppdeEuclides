655a6371b787943b94311082a6c718c2798b5b46
pxp9 <pepe.marquezromero@gmail.com>
2022-09-23 09:25:41 +0200

    update gradle

--- app/src/main/java/com/example/appdeeuclides/Main2Activity.java
+++ app/src/main/java/com/example/appdeeuclides/Main2Activity.java
@@ -2,11 +2,9 @@ package com.example.appdeeuclides;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
